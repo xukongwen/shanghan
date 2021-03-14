3a17a76508b49b0f1aec2876e3e887f29b1812a9
徐空文 <oabdao@gmail.com>
2021-03-14 08:24:14 +0800

    move to here

--- t1.py
+++ t1.py
@@ -1,45 +1,17 @@
 import streamlit as st
+import altair as alt
 import pandas as pd
 import numpy as np
 import json
 import os
 
-st.title('试验一下是吗')
+df = pd.read_csv('t.csv', sep=' ', header=None, index_col=0, names=['date', '高压', '低压'])
+st.line_chart(df)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('获取数据')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("完成! (using st.cache)")
-
-st.subheader('Raw data')
-st.write(data)
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
-
-# 这个貌似是某种万能将数据显示成表格的框架
-df = pd.DataFrame({'col1': [1,2,3]})
-df  # <-- Draw the dataframe
-
-x = 10
-'x', x  # <-- Draw the string 'x' and then the value of x
+df = pd.read_csv('t2.csv', sep=' ', header=None, names=['word', 'count'])
+df.sort_values('count', ascending=False, inplace=True)
+st.write(df)
+st.write(alt.Chart(df).mark_bar().encode(x=alt.X('word', sort=None), y='count'))
 
 
 st.title('试验json')
@@ -47,39 +19,47 @@ st.title('试验json')
 # 下面这样可以读取和显示json
 
 shdata = json.load(open(os.path.expanduser('./data/SH_ty2.json'), 'r', encoding='utf-8'))
+shdata = {(val["名"] + '-' + key): val for key, val in shdata.items()}  # dict
+# st.write(shdata)
 
 # st.json(shdata)
 
 # st.write(shdata)
 
-t2 = pd.DataFrame(shdata)
-t2
+# t2 = pd.DataFrame(shdata)
+# t2.columns = t2.iloc[0]
+# t2 = t2.iloc[1:]
+# st.write(t2)
 
-st.write(t2)
+text = st.text_area('输入查询的方子名称')
+if st.button('查询'):
+    fname = text.strip()
+    for key in shdata.keys():
+        if fname in key:
+            fdata = shdata[key]
+            st.write(fdata["证"]["体证"])
+            st.write(fdata["经"])
+            st.write(fdata["方"])
+            st.write(fdata["原文"])
+            st.markdown("**证:** " + "; ".join(["{}: {}".format(key, ",".join(val)) for key, val in fdata["证"].items()]))
+            break
+    else:
+        st.write("没有找到方剂 '%s'" % fname)
 
-st.text_area('输入查询的方子名称')
+st.title('伤寒论药物用量排名分析(前10名)')
+yao_list = []
+for fang in shdata.values():
+    yao_list.extend(fang["方"].keys())
+import collections
+c = collections.Counter(yao_list)
+data = pd.DataFrame({"fname": c.keys(), "fcount": c.values()})
+data.sort_values('fcount', ascending=False, inplace=True)
+st.write(data)
+st.write(alt.Chart(data).mark_bar().encode(x=alt.X('fname', sort=None), y='fcount'))
 
-st.button('查询')
-
-st.text_input('Enter some text')
-# checkbox
-if st.checkbox('显示内容'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    st.line_chart(chart_data)
-# 选择
-option = st.selectbox(
-    '选择',
-     df['col1'])
-
-'你选择了: ', option
-
-left_column, right_column = st.beta_columns(2)
-pressed = left_column.button('Press me?')
-if pressed:
-    right_column.write("Woohoo!")
-
-expander = st.beta_expander("显示更多")
-expander.write("更长的内容")
+fang_list = [val['名'] for val in shdata.values()]
+c = collections.Counter(fang_list)
+data = pd.DataFrame({"fname": c.keys(), "fcount": c.values()})
+data.sort_values('fcount', ascending=False, inplace=True)
+st.write(data)
+st.write(alt.Chart(data).mark_bar().encode(x=alt.X('fname', sort=None), y='fcount'))
